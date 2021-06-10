0d5121b95ed9c2ec2a0991019b81ed5efc86248f
shubham jha <shubham@juakali.io>
2021-06-10 14:49:08 +0530

    :tada: deployment build created

--- src/services/apiConfig.js
+++
@@ -1,4 +0,0 @@
-export const URLS = {
-    USERS_DATA:"https://jsonplaceholder.typicode.com/users",
-} 
-export const getAvatarURL = (username)=>`https://avatars.dicebear.com/v2/avataaars/${username}.svg?options[mood][]=happy`
